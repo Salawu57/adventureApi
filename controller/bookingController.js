4234718bce7633709f4045257fc82f8f0805da93
afeesvendy <101556554+afeesvendy@users.noreply.github.com>
2022-07-24 21:12:33 +0100

    checking error

--- controller/bookingController.js
+++ controller/bookingController.js
@@ -14,7 +14,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ["card"],
     // success_url:`${req.protocol}://${req.get('host')}/?tour=${req.params.tourID}&user=${req.user.id}&price=${tour.price}`,
-    success_url: `${req.protocol}://${req.get("host")}/`,
+    success_url: `${req.protocol}://${req.get("host")}/my-tours`,
     cancel_url: `${req.protocol}://${req.get("host")}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourID,
@@ -55,8 +55,14 @@ const createBookingCheckout = async session => {
 
   const price = session.display_items[0].amount / 100;
 
-  await Booking.create({ tour, user, price });
+  console.log(session);
+  console.log(price);
 
+  
+
+  const booking  =  await Booking.create({ tour, user, price });
+
+  console.log(booking);
 }
 
 exports.webhookCheckout = (req, res, next) => {
