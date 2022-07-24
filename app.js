db5af5fb4284ca57dfb6aa17ca80a1ec3d0c3997
afeesvendy <101556554+afeesvendy@users.noreply.github.com>
2022-07-24 19:51:45 +0100

    implement cors

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ const bookingRouter = require("./routers/bookingRouter");
 const viewRouter = require("./routers/viewRoute");
 const cookieParser = require("cookie-parser");
 const compression = require("compression");
+const cors = require('cors');
 
 const app = express();
 
@@ -29,6 +30,12 @@ app.enable('trust proxy');
 
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
+
+//implement CORS
+app.use(cors());
+
+
+app.options('*', cors());
 
 app.use(express.static(path.join(__dirname, "public")));
 
